3aa0cb91894d25fa2399de35c859ef51ee8e075a
Yash panchal <yashpanchal2905@gmail.com>
2025-10-09 00:34:11 +0530

    edited db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
+  host: process.env.SQL_HOST,
+  user: process.env.SQL_USER,
   password: process.env.SQL_PASS,
   database: process.env.SQL_DATABASE,
 });
